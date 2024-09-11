6740b6d0ad800050b445cd2e9a56be198d8feaf6
Camila <166883516+Camilacafo@users.noreply.github.com>
2024-09-11 14:39:40 -0300

    JSX

--- meu-projeto/src/App.js
+++ meu-projeto/src/App.js
@@ -2,11 +2,23 @@
 import './App.css';
 
 function App() {
+
+  const name = 'Camila'
+  const newname = name.toUpperCase()
+
+  function sum(a, b){
+    return a + b
+  }
+
+  const url = "https://www.google.com/search?q=cachorro&oq=cachorro&gs_lcrp=EgZjaHJvbWUqBwgAEAAYjwIyBwgAEAAYjwIyCggBEC4YsQMYgAQyDQgCEC4YsQMYyQMYgAQyBwgDEAAYgAQyDQgEEAAYkgMYgAQYigUyDQgFEAAYkgMYgAQYigUyCggGEC4YsQMYgAQyCggHEC4YsQMYgAQyBwgIEAAYgAQyCggJEAAYsQMYgATSAQk2MzM1ajBqMTWoAgiwAgE&sourceid=chrome&ie=UTF-8#vhid=p9KC3Z5ItgAMRM&vssid=l"
+            
   return (
     <div className="App">
-      <h1>Olá, React</h1>
-      <p>Olá, Mundo.... Estou escrevendo código em react</p>
-      <h2>Esse é o meu primeiro projeto...</h2>
+      <h2>Alterando o JSX</h2>
+      <p>Olá, {newname}.</p>
+      <p>Soma: {sum(1, 2)}</p>
+      <img src={url} alt="Minha imagem" />
+    
     </div>
   );
 }

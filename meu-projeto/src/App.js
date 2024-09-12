f1dc7147c740dc98115f712bd23b874df6c9b19d
Camila <166883516+Camilacafo@users.noreply.github.com>
2024-09-12 10:08:33 -0300

    props

--- meu-projeto/src/App.js
+++ meu-projeto/src/App.js
@@ -1,26 +1,12 @@
 
 import './App.css';
 
-import HelloWorld from './components/HelloWorld';
 
 function App() {
-
-  const name = 'Camila'
-  const newname = name.toUpperCase()
-
-  function sum(a, b){
-    return a + b
-  }
-
-  const url = "https://www.google.com/search?q=cachorro&oq=cachorro&gs_lcrp=EgZjaHJvbWUqBwgAEAAYjwIyBwgAEAAYjwIyCggBEC4YsQMYgAQyDQgCEC4YsQMYyQMYgAQyBwgDEAAYgAQyDQgEEAAYkgMYgAQYigUyDQgFEAAYkgMYgAQYigUyCggGEC4YsQMYgAQyCggHEC4YsQMYgAQyBwgIEAAYgAQyCggJEAAYsQMYgATSAQk2MzM1ajBqMTWoAgiwAgE&sourceid=chrome&ie=UTF-8#vhid=p9KC3Z5ItgAMRM&vssid=l/150x150"
-            
+        
   return (
     <div className="App">
-      <h2>Alterando o JSX</h2>
-      <p>Olá, {newname}.</p>
-      <p>Soma: {sum(1, 2)}</p>
-      <img src={url} alt="Minha imagem" />
-      <HelloWorld/>
+      
     
     </div>
   );

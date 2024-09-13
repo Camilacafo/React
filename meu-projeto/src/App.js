ee72e283137c76b6c00238bbab123dbd1e8951ee
Camila <166883516+Camilacafo@users.noreply.github.com>
2024-09-13 14:35:40 -0300

    Tentando entender o problema na importação

--- meu-projeto/src/App.js
+++ meu-projeto/src/App.js
@@ -1,6 +1,7 @@
 
 import './App.css'
 import SayMyName from './components/SayMyName'
+import Pessoa from './components/Pessoa'
 
 
 
@@ -10,7 +11,10 @@ function App() {
     <div className="App">
       <SayMyName nome = "Camila"/>
       <SayMyName nome = {nome}/> 
-      
+      <Pessoa nome="Camila" 
+      idade="31"
+      profissão="Progamadora fullstack"
+      foto="https://via.placeholder.com/150"/>
 
 
     </div>
